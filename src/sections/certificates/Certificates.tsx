54670e542c144ab7005db53acdc9dc178d151d6a
ahmed tarek <ahmedtareka777@gmail.com>
2024-09-15 13:54:33 +0300

    add more hijama certificates

--- src/sections/certificates/Certificates.tsx
+++ src/sections/certificates/Certificates.tsx
@@ -12,7 +12,7 @@ import CertificatesSliderHolder, {
 // utils
 import { nanoid } from "nanoid";
 
-// imgs
+// imgs \\
 
 // AUPS
 import AUPS_1 from "../../../imgs/certificates/AUPS/1.jpg";
@@ -23,6 +23,10 @@ import AUPS_3 from "../../../imgs/certificates/AUPS/3.jpg";
 import hejamaImg from "../../../imgs/certificates/hejama/1.webp";
 import hejamaImg_2 from "../../../imgs/certificates/hejama/2.jpg";
 import hejamaImg_3 from "../../../imgs/certificates/hejama/3.png";
+import hejamaImg_4 from "../../../imgs/certificates/hejama/4.webp";
+import hejamaImg_5 from "../../../imgs/certificates/hejama/5.webp";
+import hejamaImg_6 from "../../../imgs/certificates/hejama/6.jpg";
+import hejamaImg_7 from "../../../imgs/certificates/hejama/7.jpg";
 
 // bee venom
 import bee_venom from "../../../imgs/certificates/bee_venom/1.svg";
@@ -47,25 +51,53 @@ import card_5 from "../../../imgs/certificates/cards/5.jpg";
 import card_6 from "../../../imgs/certificates/cards/6.jpg";
 import card_7 from "../../../imgs/certificates/cards/7.svg";
 
-// plants and woam medicines
+// plants
+import plants_1 from "../../../imgs/certificates/plants/1.jpg";
+import plants_2 from "../../../imgs/certificates/plants/2.jpg";
+
+// pins
+import pins_1 from "../../../imgs/certificates/pins/1.jpg";
+
+// colors
+import colors_1 from "../../../imgs/certificates/colors.jpg";
+
+// appreciation
+import appreciation_1 from "../../../imgs/certificates/appreciation.jpg";
+
+// woam
 import worm from "../../../imgs/certificates/worm.jpg";
-import plants from "../../../imgs/certificates/plants.jpg";
 
 const AUPS = [AUPS_1, AUPS_2, AUPS_3];
-const hijamaImgs = [hejamaImg, hejamaImg_2, hejamaImg_3];
+const hijamaImgs = [
+  hejamaImg,
+  hejamaImg_2,
+  hejamaImg_3,
+  hejamaImg_4,
+  hejamaImg_5,
+  hejamaImg_6,
+  hejamaImg_7,
+];
 const beeVenom = [bee_venom, bee_venom_2];
 const massageAndFirstAidImgs = [img_1, img_2, img_3, img_4];
 const training = [training_1, training_2];
 const cards = [card_1, card_2, card_3, card_4, card_5, card_6, card_7];
-const wormAndPlants = [worm, plants];
+const wormArr = [worm];
+const plants = [plants_1, plants_2];
+const pins = [pins_1];
+const colors = [colors_1];
+const appreciation = [appreciation_1];
 
 const imgsList = [
   ...AUPS,
   ...hijamaImgs,
   ...beeVenom,
-  ...wormAndPlants,
+  ...plants,
+  ...pins,
+  ...colors,
+  ...appreciation,
   ...massageAndFirstAidImgs,
   ...training,
+  ...wormArr,
   ...cards,
 ];
 
